868d6b75315b82b1da30520543e72dbcbd2de5a6
dfitriana <diyahayufa@students.unnes.ac.id>
2021-05-09 21:22:33 +0700

    edit navbar

--- resources/views/Layout/operator.blade.php
+++ resources/views/Layout/operator.blade.php
@@ -49,8 +49,8 @@
                                         <div class="col-lg-12 px-2">
                                             <div class="submenu-box"> 
                                                 <ul class="list-unstyled m-0">
-                                                    <li><a href="">Home</a></li>
-                                                    <li><a href="">Profil Pengusul</a></li>
+                                                    <li><a href="{{route('operator-home')}}">Home</a></li>
+                                                    <li><a href="{{route('profil-pengusul')}}">Profil Pengusul</a></li>
                                                 </ul>
                                             </div>
                                         </div>
@@ -116,7 +116,49 @@
         </div>
         <!-- Fin sidebar -->
 
-    @yield('content')
+    <div id="page-content-wrapper" class="w-100 bg-light-blue">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+            <div class="container">
+                <!-- <button class="btn btn-primary text-primary" id="menu-toggle">Menu</button> -->
+                <div type="button"  id="menu-toggle" class="nav-icon1 hamburger animated fadeInLeft is-closed" data-toggle="offcanvas">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link text-dark dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Habib Taqiyullah
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <br>
+                                <div class="text-center">
+                                    <img class="img-profile rounded-circle text-center" src="assets/img/habib.png" width="50px">
+                                </div>
+                                <h5 class="dropdown-header text-center">Habib Taqiyullah</h5>
+                                <h6 class="dropdown-header text-center" style="font-weight: bold;">Operator</h6>
+                                <div class="dropdown-divider"></div>
+                                <div class="text-center">
+                                    <button type="button" class="btn btn-danger btn-sm right" id="logout">Log Out</button>
+                                </div>
+                                
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        @yield('content')
+    </div>
+
+    
   </div>
   <!-- Fin wrapper -->
 
