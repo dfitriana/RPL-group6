1a645245d315bce6d1aec1fabb693316b6c6b5ea
dfitriana <diyahayufa@students.unnes.ac.id>
2021-05-09 21:29:16 +0700

    menambahkan controller profil pengusul dan edit profil pengusul

--- app/Http/Controllers/OperatorController.php
+++ app/Http/Controllers/OperatorController.php
@@ -11,8 +11,20 @@ use Illuminate\Support\Facades\DB;
 class OperatorController extends Controller
 {
     public function operator(){
+        
         $users = User::all();
-
         return view('Operator.dashboard', compact('users'));
+    }
+
+    public function profilpengusul(){
+        
+        $users = User::all();
+        return view('Operator.profil-pengusul', compact('users'));
+    }
+
+    public function editprofilpengusul(){
+        
+        $users = User::all();
+        return view('Operator.edit-profil-pengusul', compact('users'));
     }
 }
