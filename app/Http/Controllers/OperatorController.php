4c350367df341e51a66c6c189def2702cc29944a
dfitriana <diyahayufa@students.unnes.ac.id>
2021-05-09 23:07:10 +0700

    menambahkan controller untuk kerjasamapendidikan, kualitas input mahasiswa & sdmewmpdtps

--- app/Http/Controllers/OperatorController.php
+++ app/Http/Controllers/OperatorController.php
@@ -27,4 +27,22 @@ class OperatorController extends Controller
         $users = User::all();
         return view('Operator.edit-profil-pengusul', compact('users'));
     }
+
+    public function kerjasamapendidikan(){
+        
+        $users = User::all();
+        return view('Operator.Kerjasama.kerjasama-pendidikan', compact('users'));
+    }
+
+    public function kualitasinputmahasiswa(){
+        
+        $users = User::all();
+        return view('Operator.Mahasiswa.kualitas-input-mahasiswa', compact('users'));
+    }
+
+    public function sdmewmpdtps(){
+        
+        $users = User::all();
+        return view('Operator.SDM.EWMP-DTPS', compact('users'));
+    }
 }
