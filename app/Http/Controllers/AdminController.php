741a3dc6e445cca37664cfd7a1cca5a2988cc24c
muhabibta <habibtaqiyullah9@students.unnes.ac.id>
2021-05-10 20:03:53 +0700

    Setting kode periode pada Controller

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\Helper;
 use App\Models\User;
+use App\Models\Periode;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
@@ -18,10 +20,11 @@ class AdminController extends Controller
 
     public function periode()
     {
+        $kode_periode = Helper::IDGenerator(new Periode, 'kode_periode', 7, 'PKK');
         $users = User::all();
         $program = Http::withBasicAuth('webmipa', 'k4cangg0r3ngr3ny4h')->get('http://services.unnes.ac.id/api/listprodi/4');
         $programData = $program['data'];
-        return view('admin.penetapan-periode', compact('users', 'programData'));
+        return view('admin.penetapan-periode', compact('users', 'programData', 'kode_periode'));
     }
 
     public function plotting()
