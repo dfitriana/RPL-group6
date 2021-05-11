06837527f7970c28701a72944185115f67124923
muhabibta <habibtaqiyullah9@students.unnes.ac.id>
2021-05-11 10:52:05 +0700

    Menambahkan data user pada UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,12 +16,28 @@ class UserSeeder extends Seeder
     public function run()
     {
         User::truncate();
-        User::create([
-            'name' => 'Administrator',
-            'level' => 'admin',
-            'email' => 'admin@gmail.com',
-            'password' => bcrypt('1234'),
-            'remember_token' => Str::random(60),
-        ]);
+        // User::create([
+        //     'name' => 'Administrator',
+        //     'level' => 'admin',
+        //     'email' => 'admin@gmail.com',
+        //     'password' => bcrypt('1234'),
+        //     'remember_token' => Str::random(60),
+        // ]);
+        DB::table('users')->insert([
+            [
+                'name' => 'Purwanto',
+                'level' => 'Administrator',
+                'email' => 'admin@gmail.com',
+                'password' => bcrypt('1234'),
+                'remember_token' => Str::random(60),
+            ],
+            [
+                'name' => 'Alfat Motorik',
+                'level' => 'Operator',
+                'email' => 'operator@gmail.com',
+                'password' => bcrypt('1234'),
+                'remember_token' => Str::random(60),
+            ]
+            ]);
     }
 }
