cee6a88e0cd7a8b147bd1f5ca965451fc8371d57
dfitriana <diyahayufa@students.unnes.ac.id>
2021-05-11 21:38:28 +0700

    mengganti auth user pada OperatorController

--- app/Http/Controllers/OperatorController.php
+++ app/Http/Controllers/OperatorController.php
@@ -6,43 +6,45 @@ use App\Models\User;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+
 
 
 class OperatorController extends Controller
 {
     public function operator(){
         
-        $users = User::all();
+        $users = Auth::user();
         return view('Operator.dashboard', compact('users'));
     }
 
     public function profilpengusul(){
         
-        $users = User::all();
+        $users = Auth::user();
         return view('Operator.profil-pengusul', compact('users'));
     }
 
     public function editprofilpengusul(){
         
-        $users = User::all();
+        $users = Auth::user();
         return view('Operator.edit-profil-pengusul', compact('users'));
     }
 
     public function kerjasamapendidikan(){
         
-        $users = User::all();
+        $users = Auth::user();
         return view('Operator.Kerjasama.kerjasama-pendidikan', compact('users'));
     }
 
     public function kualitasinputmahasiswa(){
         
-        $users = User::all();
+        $users = Auth::user();
         return view('Operator.Mahasiswa.kualitas-input-mahasiswa', compact('users'));
     }
 
     public function sdmewmpdtps(){
         
-        $users = User::all();
+        $users = Auth::user();
         return view('Operator.SDM.EWMP-DTPS', compact('users'));
     }
 }
