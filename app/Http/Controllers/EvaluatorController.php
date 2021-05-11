77fd6b83a945e101c5653382ea4a0d635887d9ec
dfitriana <diyahayufa@students.unnes.ac.id>
2021-05-11 21:38:14 +0700

    mengganti auth user pada EvaluatorController

--- app/Http/Controllers/EvaluatorController.php
+++ app/Http/Controllers/EvaluatorController.php
@@ -4,13 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
 
 
 class EvaluatorController extends Controller
 {
     //
     public function evaluator(){
-        $users = User::all();
+        $users = Auth::user();
         return view('Evaluator.dashboard', compact('users'));
 
     }
