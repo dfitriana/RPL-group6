2d910da9adfaa5081aaaec10ef4617083d33d30a
muhabibta <habibtaqiyullah9@students.unnes.ac.id>
2021-06-08 00:19:40 +0700

    Membuat migration data dosen

---
+++ database/migrations/2021_06_07_155256_create_data_dosens_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDataDosensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('data_dosens', function (Blueprint $table) {
+            $table->id('nip');
+            $table->string('nama', 50);
+            $table->string('email', 50)->nullable();
+            $table->integer('jabatan_id')->nullable();
+            $table->integer('kode_unit')->nullable();
+            $table->integer('prodi_doskar')->nullable();
+            $table->string('mail_unnes')->nullable();
+            $table->string('bidang_ilmu')->nullable();
+            $table->integer('prodi_jabatan')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('data_dosens');
+    }
+}
