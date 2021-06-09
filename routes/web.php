352722f0b05a9c543f15f4ea316041294a3e49ed
muhabibta <habibtaqiyullah9@students.unnes.ac.id>
2021-06-09 22:52:47 +0700

    Setting route tampilan

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,7 @@ Route::get('/operator-dashboard', [OperatorController::class, 'operator'])->name
 Route::get('/operator-home-mahasiswa',[OperatorController::class,'mahasiswa'])->name('home-mahasiswa');
 Route::get('/operator-home-sdm',[OperatorController::class,'sdm'])->name('home-sdm');
 Route::get('/operator-home-keuangansarpras',[OperatorController::class,'keuanganhome'])->name('home-keuangan');
+Route::get('/operator-home-pendidikan',[OperatorController::class,'pendidikan'])->name('home-pendidikan');
 //operator-dashboard-profil pengusul
 Route::get('/operator-profilPengusul', [OperatorController::class, 'profilpengusul'])->name('profil-pengusul');
 Route::get('/operator-editProfilPengusul', [OperatorController::class, 'editprofilpengusul'])->name('edit-profil-pengusul');
