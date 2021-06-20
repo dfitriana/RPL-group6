a25f06b7349c02435c32f29a01dc78eff2edf1e4
muhabibta <habibtaqiyullah9@students.unnes.ac.id>
2021-06-20 20:02:00 +0700

    Setting notif pada controller

--- app/Http/Controllers/EvaluatorController.php
+++ app/Http/Controllers/EvaluatorController.php
@@ -2,12 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Notification;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
-
 
 class EvaluatorController extends Controller
 {
@@ -16,5 +16,17 @@ class EvaluatorController extends Controller
         $users = Auth::user();
         return view('Evaluator.dashboard', compact('users'));
 
+    }
+
+    public function penugasan(){
+        $users = Auth::User();
+        $notifs= Notification::where('notifiable_id',$users->id)->get()->take(1);
+        return view('Evaluator.penugasan',compact('users','notifs'));
+    }
+
+    public function detail($id){
+        $users = Auth::User();
+        $notifs = Notification::where('id',$id)->get();
+        return view('Evaluator.penugasan', compact('users','notifs'));
     }
 }
