1b90e9b30fed3ff0ea4de4cd9b7a1642d3e8545e
muhabibta <habibtaqiyullah9@students.unnes.ac.id>
2021-07-03 11:43:03 +0700

    Menambahkan method tolak dan menerima

--- app/Http/Controllers/EvaluatorController.php
+++ app/Http/Controllers/EvaluatorController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Evaluator;
 use App\Models\Notification;
+use App\Models\plotting;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -12,21 +14,83 @@ use Illuminate\Support\Facades\Http;
 class EvaluatorController extends Controller
 {
     //
-    public function evaluator(){
+    public function evaluator()
+    {
         $users = Auth::user();
         return view('Evaluator.dashboard', compact('users'));
-
     }
 
-    public function penugasan(){
+    public function penugasan()
+    {
         $users = Auth::User();
-        $notifs= Notification::where('notifiable_id',$users->id)->get()->take(1);
-        return view('Evaluator.penugasan',compact('users','notifs'));
+        $notifs = Notification::where('notifiable_id', $users->id)
+            ->where('read_at', null)
+            ->get()
+            ->take(1);
+        return view('Evaluator.penugasan', compact('users', 'notifs'));
     }
 
-    public function detail($id){
+    public function detail($id)
+    {
         $users = Auth::User();
-        $notifs = Notification::where('id',$id)->get();
-        return view('Evaluator.penugasan', compact('users','notifs'));
+        $notifs = Notification::where('id', $id)->get();
+        return view('Evaluator.penugasan', compact('users', 'notifs'));
+    }
+
+    public function setuju($id)
+    {
+        $users = Auth::User();
+        $userUnreadNotification = $users
+            ->unreadNotifications
+            ->where('id', $id)
+            ->first();
+
+        if ($userUnreadNotification) {
+            $userUnreadNotification->markAsRead();
+        }
+        $data = Notification::where('id', $id)->value('data');
+        $inner = $data['data'];
+        $ideva = plotting::where('kode_periode', $inner['kode_periode'])
+            ->where('nip', $users->nip)
+            ->value('id_evaluator');
+
+        $confirm = new Evaluator;
+        $confirm->kode_periode = $inner['kode_periode'];
+        $confirm->id_evaluator = $ideva;
+        $confirm->nip          = $users->nip;
+        $confirm->is_confirm   = 'Yes';
+        $confirm->save();
+
+
+        return redirect('/evaluator-dashboard')->with('info', 'Data telah dicatat');
+    }
+
+    public function tolak(Request $request, $id)
+    {
+        $users = Auth::User();
+        $userUnreadNotification = $users
+            ->unreadNotifications
+            ->where('id', $id)
+            ->first();
+
+        if ($userUnreadNotification) {
+            $userUnreadNotification->markAsRead();
+        }
+        $data = Notification::where('id', $id)->value('data');
+        $inner = $data['data'];
+        $ideva = plotting::where('kode_periode', $inner['kode_periode'])
+            ->where('nip', $users->nip)
+            ->value('id_evaluator');
+
+        $confirm = new Evaluator;
+        $confirm->kode_periode = $inner['kode_periode'];
+        $confirm->id_evaluator = $ideva;
+        $confirm->nip          = $users->nip;
+        $confirm->is_confirm   = 'No';
+        $confirm->alasan       = $request->alasan;
+        $confirm->save();
+
+
+        return redirect('/evaluator-dashboard')->with('info', 'Data telah dicatat');
     }
 }
