dc3d0f3d90d9a017f9f62e585a0f1bfeda194037
muhabibta <habibtaqiyullah9@students.unnes.ac.id>
2021-07-03 11:38:10 +0700

    Setting tabel evaluator

---
+++ database/migrations/2021_07_03_040622_create_evaluators_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEvaluatorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('evaluators', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode_periode');
+            $table->integer('id_evaluator');
+            $table->integer('nip');
+            $table->enum('is_confirm',['Yes','No']);
+            $table->string('alasan',225);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('evaluators');
+    }
+}
